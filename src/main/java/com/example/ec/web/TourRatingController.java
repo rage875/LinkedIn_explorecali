b8d5ca17e6e46c9b56fdd484e7cec84ed5e668b0
rage875 <rage.875@gmail.com>
2021-09-13 16:44:51 -0500

    Spring MVC REST: POST Method

--- src/main/java/com/example/ec/web/TourRatingController.java
+++ src/main/java/com/example/ec/web/TourRatingController.java
@@ -1,16 +1,27 @@
 package com.example.ec.web;
 
+import com.example.ec.domain.Tour;
+import com.example.ec.domain.TourRating;
+import com.example.ec.domain.TourRatingPk;
 import com.example.ec.repository.TourRatingRepository;
 import com.example.ec.repository.TourRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.NoSuchElementException;
+
+/**
+ * Tour Rating Controller
+ *
+ * Created by Mary Ellen Bowman
+ */
 @RestController
 @RequestMapping(path = "/tours/{tourId}/ratings")
 public class TourRatingController {
-    TourRatingRepository tourRatingRepository;
-    TourRepository tourRepository;
+    private TourRatingRepository tourRatingRepository;
+    private TourRepository tourRepository;
 
     @Autowired
     public TourRatingController(TourRatingRepository tourRatingRepository, TourRepository tourRepository) {
@@ -19,6 +30,45 @@ public class TourRatingController {
     }
 
     protected TourRatingController() {
+
+    }
+
+    /**
+     * Create a Tour Rating.
+     *
+     * @param tourId tour identifier
+     * @param ratingDto rating data transfer object
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createTourRating(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+        Tour tour = verifyTour(tourId);
+        tourRatingRepository.save(new TourRating( new TourRatingPk(tour, ratingDto.getCustomerId()),
+                ratingDto.getScore(), ratingDto.getComment()));
+    }
+
+    /**
+     * Verify and return the Tour given a tourId.
+     *
+     * @param tourId tour identifier
+     * @return the found Tour
+     * @throws NoSuchElementException if no Tour found.
+     */
+    private Tour verifyTour(int tourId) throws NoSuchElementException {
+        return tourRepository.findById(tourId).orElseThrow(() ->
+                new NoSuchElementException("Tour does not exist " + tourId));
+    }
+
+    /**
+     * Exception handler if NoSuchElementException is thrown in this Controller
+     *
+     * @param ex exception
+     * @return Error message String.
+     */
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public String return400(NoSuchElementException ex) {
+        return ex.getMessage();
 
     }
 
